6c7ce306480f703702a7548a62e0f72f0b41f9ad
Amja Do <amri.amjado@gmail.com>
2024-12-15 18:19:34 +0100

    change connection string

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static final String URL = "jdbc:mysql://host.docker.internal:3306/library_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     private static Connection connection;
